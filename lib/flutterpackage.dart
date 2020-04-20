4c9680503a1986050c6bd6a2f0c35661e12a22ab
Samuel Ezedi <samuelezedi@gmail.com>
2020-04-20 01:05:31 +0100

    second commit

--- lib/flutterpackage.dart
+++ lib/flutterpackage.dart
@@ -1,7 +1,106 @@
 library flutterpackage;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutterpackage/my_navigator.dart';
+import 'package:flutterpackage/walkthrough.dart';
+
+class IntroScreen extends StatefulWidget {
+  @override
+  IntroScreenState createState() {
+    return IntroScreenState();
+  }
 }
+
+class IntroScreenState extends State<IntroScreen> {
+  final PageController controller = new PageController();
+  int currentPage = 0;
+  bool lastPage = false;
+
+  void _onPageChanged(int page) {
+    setState(() {
+      currentPage = page;
+      if (currentPage == 3) {
+        lastPage = true;
+      } else {
+        lastPage = false;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFFEEEEEE),
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Expanded(
+            flex: 3,
+            child: PageView(
+              children: <Widget>[
+                Walkthrough(
+                  title: "TITLE",
+                  content: "CONTENT",
+                  imageIcon: Icons.mobile_screen_share,
+                ),
+                Walkthrough(
+                  title: "TITLE",
+                  content: "CONTENT",
+                  imageIcon: Icons.search,
+                ),
+                Walkthrough(
+                  title: "TITLE",
+                  content: "CONTENT",
+                  imageIcon: Icons.shopping_cart,
+                ),
+                Walkthrough(
+                  title: "TITLE",
+                  content: "CONTENT",
+                  imageIcon: Icons.verified_user,
+                ),
+              ],
+              controller: controller,
+              onPageChanged: _onPageChanged,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                FlatButton(
+                  child: Text(lastPage ? "" : "SKIP",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0)),
+                  onPressed: () =>
+                  lastPage ? null : MyNavigator.goToHome(context),
+                ),
+                FlatButton(
+                  child: Text(lastPage ? "GOT IT" : "NEXT",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0)),
+                  onPressed: () => lastPage
+                      ? MyNavigator.goToHome(context)
+                      : controller.nextPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeIn),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
